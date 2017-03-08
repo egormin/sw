05dc5de07d3caaaa382266ab62cf86ef95920088
Egor <poseidon@gmail.com>
2017-03-08 21:37:39 +0300

    only with sublime3 installer

--- subl3Install.sh
+++ subl3Install.sh
@@ -4,13 +4,13 @@ echo "#####SUBLIME 3 SOFTWARE INSTALLATION#####"
 
 sleep 1
 
-echo "STAGE1: Software downloading"
+echo "STAGE 1: Software downloading"
 wget https://download.sublimetext.com/sublime_text_3_build_3126_x64.tar.bz2
 
-echo "STAGE2: Unpacking (to /opt)"
+echo "STAGE 2: Unpacking (to /opt)"
 tar -vxjf sublime_text_3_build_3126_x64.tar.bz2 -C /opt
 
-echo "STAGE3: Link creation"
+echo "STAGE 3: Link creation"
 ln -s /opt/sublime_text_3/sublime_text /usr/bin/subl
 
 echo "Subilme 3 has been installed successfully"
